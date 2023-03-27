972315f93e7412b797b66d32d68bab2004c03538
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-26 21:03:20 -0500

    vsc wanted another underscore

--- amplify/backend/function/servproj3/src/app.js
+++ amplify/backend/function/servproj3/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
-app.use(function(req, res, next) {
+app.use(function(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "*")
   next()
